5d6c65dc249a54b721bfcb9a10d89184623c510b
Drakkarrr <junrey404@gmail.com>
2023-10-22 05:41:03 +0800

    langding revamp

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+import Image from "next/image";
+import Logo from "@/assets/logo.png";
+import { Button } from "@/components/ui/button";
+
 export default function WelcomePage() {
   return (
-    <div>
-      <h1>Welcome to MOELCI-II Bonicafio</h1>
+    <div className="grid w-full h-full bg-orange-400 place-items-center">
+      <div>
+        <Image src={Logo} width={200} height={200} alt="Moelci-II logo" />
+      </div>
+      <div className="text-center">
+        <h1 className="font-bold text-red-600 uppercase">
+          Welcome to moelci-ii
+        </h1>
+        <h2>Bonifacio Branch</h2>
+        <Button>Proceed</Button>
+      </div>
     </div>
   );
 }
