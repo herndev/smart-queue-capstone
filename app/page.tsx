f204727e91742802643dff919ae3f50d27ec1327
Drakkarrr <junrey404@gmail.com>
2023-10-22 11:19:23 +0800

    constructed landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,42 @@
-import Image from "next/image";
-import Logo from "@/assets/logo.png";
-import { Button, buttonVariants } from "@/components/ui/button";
-import Link from "next/link";
-import { cn } from "@/lib/utils";
+import Image from "next/image"
+import Logo from "@/assets/logo.png"
+import WavesBg from "@/assets/bg-svg.png"
+import { buttonVariants } from "@/components/ui/button"
+import Link from "next/link"
+import { cn } from "@/lib/utils"
 
 export default function WelcomePage() {
   return (
-    <div className="grid w-full h-full bg-orange-400 place-items-center">
-      <div>
-        <Image src={Logo} width={200} height={200} alt="Moelci-II logo" />
-      </div>
+    <div className="grid w-full h-full pt-4 place-items-center">
+      <Image src={Logo} width={200} height={200} alt="Moelci-II logo" />
       <div className="text-center">
-        {/* <h1 className="font-bold text-red-600 uppercase">
-          Welcome to moelci-ii
-        </h1> */}
-        <h1 className="text-3xl font-heading sm:text-5xl md:text-6xl lg:text-7xl">
+        <h1 className="p-6 text-3xl font-extrabold text-[#E80203] font-heading sm:text-5xl md:text-6xl lg:text-7xl">
           WELCOME TO MOELCI-II
         </h1>
-        <h2 className="font-heading text-3xl leading-[1.1] sm:text-3xl md:text-6xl">
+        <h2 className="font-heading text-3xl font-bold leading-[1.1] sm:text-3xl md:text-6xl">
           Bonifacio Branch
         </h2>
-        <div className="space-x-4">
-          <Link href="/purpose" className={cn(buttonVariants({ size: "lg" }))}>
+        <div className="pt-8 space-x-4 ">
+          <Link
+            href="/purpose"
+            className={`text-xl tracking-wide font-extrabold bg-[#F9D029] hover:bg-yellow-300 text-black ${cn(
+              buttonVariants({ size: "lg" })
+            )}`}
+          >
             Proceed
           </Link>
-          {/* <p>{currentDate}</p>
-            <p>{currentTime}</p> */}
+        </div>
+      </div>
+      <div className="grid w-full h-full mt-20 grid-row-2">
+        <div className="bg-[#F9D029]">
+          <p>&nbsp;</p>
+          <p>&nbsp;</p>
+        </div>
+        <div className="bg-[#E80203]">
+          <p>&nbsp;</p>
+          <p>&nbsp;</p>
         </div>
       </div>
     </div>
-  );
+  )
 }
