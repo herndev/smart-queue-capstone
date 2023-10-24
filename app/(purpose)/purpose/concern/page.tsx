e8f6ea62753f0b0fdb11f4b29a8809aa74ed7d07
Drakkarrr <junrey404@gmail.com>
2023-10-24 08:46:11 +0800

    concern page finished

--- app/(purpose)/purpose/concern/page.tsx
+++ app/(purpose)/purpose/concern/page.tsx
@@ -1,5 +1,59 @@
-import React from 'react';
+import Image from "next/image"
+import Logo from "@/assets/logo.png"
+import { buttonVariants } from "@/components/ui/button"
+import Link from "next/link"
+import { cn } from "@/lib/utils"
 
 export default function ConcernPage() {
-  return <div>Concern Page Print</div>;
+  return (
+    <div className="grid w-full h-full place-items-center">
+      <div className="grid w-full h-full grid-row-2">
+        <div className="bg-[#E80203]">
+          <p>&nbsp;</p>
+          <p>&nbsp;</p>
+          <p>&nbsp;</p>
+          <p>&nbsp;</p>
+          <p>&nbsp;</p>
+          <p>&nbsp;</p>
+        </div>
+        <div className="bg-[#F9D029]">
+          <p>&nbsp;</p>
+          <p>&nbsp;</p>
+          <p>&nbsp;</p>
+        </div>
+      </div>
+      <div className="grid w-10/12 p-8 space-y-10 ring ring-black ring-offset-2 ring-offset-slate-50 dark:ring-offset-slate-900 place-items-center">
+        <Image
+          src={Logo}
+          width={200}
+          height={200}
+          alt="Moelci-II logo"
+          className="absolute top-24"
+        />
+        <div className="w-full space-y-32 text-center">
+          <h2 className="font-heading text-3xl font-bold bg-[#F9D029] w-96 py-4 m-auto rounded-3xl leading-[1.1] sm:text-3xl md:text-6xl">
+            Concern
+          </h2>
+          <div className="flex justify-around w-full">
+            <Link
+              href="/purpose/concern"
+              className={`rounded-none text-xl tracking-wide font-bold bg-black hover:pb-2 text-white ${cn(
+                buttonVariants({ size: "lg" })
+              )}`}
+            >
+              &larr; Back
+            </Link>
+            <Link
+              href="/purpose/payment"
+              className={`rounded-none text-xl tracking-wide font-bold hover:pb-2 text-white ${cn(
+                buttonVariants({ size: "lg", variant: "destructive" })
+              )}`}
+            >
+              Print Q. number
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
