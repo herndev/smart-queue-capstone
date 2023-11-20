c0ab96277aa8469bd5453277ccb332b5c7f6de26
Drakkarrr <junrey404@gmail.com>
2023-11-20 18:54:17 +0800

    built login form for admin

--- app/(teller)/admin/page.tsx
+++ app/(teller)/admin/page.tsx
@@ -19,19 +19,21 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 
-export function CardWithForm() {
+export default function CardWithForm() {
   return (
     <Card className="w-[350px]">
-      <CardHeader>
-        <CardTitle>Create project</CardTitle>
-        <CardDescription>Deploy your new project in one-click.</CardDescription>
+      <CardHeader className="mx-auto">
+        <CardTitle>Welcome Admin!</CardTitle>
+        <CardDescription>
+          Please login you credentials to continue
+        </CardDescription>
       </CardHeader>
       <CardContent>
         <form>
           <div className="grid items-center w-full gap-4">
             <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Name</Label>
-              <Input id="name" placeholder="Name of your project" />
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" placeholder="admin@moelcibonifacio.com" />
             </div>
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="framework">Framework</Label>
@@ -52,7 +54,7 @@ export function CardWithForm() {
       </CardContent>
       <CardFooter className="flex justify-between">
         <Button variant="outline">Cancel</Button>
-        <Button>Deploy</Button>
+        <Button>Login</Button>
       </CardFooter>
     </Card>
   );
