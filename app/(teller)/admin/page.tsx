344d4a9e850cedbc92c5de81a6a50369d7dcbac6
Drakkarrr <junrey404@gmail.com>
2023-11-20 18:26:52 +0800

    build initial account form for admin

--- app/(teller)/admin/page.tsx
+++ app/(teller)/admin/page.tsx
@@ -1,7 +1,18 @@
-export default function TellerPage() {
+import { Separator } from "@/components/ui/separator";
+import { AccountForm } from "@/components/account-form";
+
+export default function AdminPage() {
   return (
-    <div>
-      <h1>Teller/Admin Login page here</h1>
+    <div className="space-y-6">
+      <div>
+        <h3 className="text-lg font-medium">Account</h3>
+        <p className="text-sm text-muted-foreground">
+          Update your account settings. Set your preferred language and
+          timezone.
+        </p>
+      </div>
+      <Separator />
+      <AccountForm />
     </div>
   );
 }
